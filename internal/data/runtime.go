97c5b2571fe19800fb524933557721297e191c4f
amir mahmoodnia <mahmoodnia.am@gmail.com>
2023-05-17 13:04:16 +0330

    make the Runtime type unmarshaler too so we can pass to it "<runtime> mins" instead of just <runtime> number.

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 // Declare a custom runtime type with underlying type of int32.
 type Runtime int32
@@ -17,4 +21,27 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	// It should be surrounded by double quotes.
 	quotedJSONValue := strconv.Quote(jsonValue)
 	return []byte(quotedJSONValue), nil
+}
+
+// This will change the Runtime and so we make it pointer receiver
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	// The jsonValue is in the format of "<runtime> mins"
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJSONValue, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	// convert to int32
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+	*r = Runtime(i)
+	return nil
 }
