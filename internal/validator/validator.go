2c84edb4f75ffc97e8507f605ea89773ef97412a
amir mahmoodnia <mahmoodnia.am@gmail.com>
2023-05-17 18:12:22 +0330

    add the validator package to validate the data entered by the client user.

---
+++ internal/validator/validator.go
@@ -0,0 +1,54 @@
+package validator
+
+import "regexp"
+
+var (
+	// For checking the format of email address.
+	EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+)
+
+// A type which contain a map of validation errors.
+type Validator struct {
+	Errors map[string]string
+}
+
+func New() *Validator {
+	return &Validator{Errors: make(map[string]string)}
+}
+
+func (v *Validator) Valid() bool {
+	return len(v.Errors) == 0
+}
+
+func (v *Validator) AddError(key, messgage string) {
+	if _, exists := v.Errors[key]; !exists {
+		v.Errors[key] = messgage
+	}
+}
+
+func (v *Validator) Check(ok bool, key, message string) {
+	if !ok {
+		v.AddError(key, message)
+	}
+}
+
+func In(value string, list ...string) bool {
+	for _, v := range list {
+		if value == v {
+			return true
+		}
+	}
+	return false
+}
+
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
+}
+
+func Unique(values []string) bool {
+	uniqueValues := make(map[string]bool)
+	for _, value := range values {
+		uniqueValues[value] = true
+	}
+	return len(uniqueValues) == len(values)
+}
