368aad50c4779c09c6a2e2569573aad5ae6d468b
amir mahmoodnia <mahmoodnia.am@gmail.com>
2023-05-18 19:22:00 +0330

    establish a connection to our postgres database through lib/pq driver and using the GREENLIGHT_DB_DSN environment varaible.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 // Application version number.
@@ -18,6 +22,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 // Holds dependencies for HTTP handlers, helpers, middleware.
@@ -34,10 +41,19 @@ func main() {
 	//our config struct.
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "Postgresql DSN")
 	flag.Parse()
 
 	// simple logger which write into stdout and prefix with local date and time.
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
+
+	logger.Printf("database connection pool established.")
 
 	// Declare an instance of application struct and fill the config and logger fields of it.
 	app := &application{
@@ -55,7 +71,24 @@ func main() {
 
 	// Start the HTTP server and log it on stdout.
 	logger.Printf("starting %s server on %d", cfg.env, cfg.port)
-	if err := srv.ListenAndServe(); err != nil {
-		logger.Fatal(err)
+	err = srv.ListenAndServe()
+	logger.Fatal(err)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
 	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Stablish a connection passing the above ctx as a parameter
+	// If the connection couldn't be estblished successfully within the deadline above
+	// this will return error
+	if err := db.PingContext(ctx); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
