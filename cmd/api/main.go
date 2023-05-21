c1c65995e3d2d15bd66faf926a3c956c054de22c
amir mahmoodnia <mahmoodnia.am@gmail.com>
2023-05-21 21:04:03 +0330

    embed a interface with four function Insert, Get, Update, Delete into a Models struct and add it to the application struct . using the interface so we can mock the embedded models too.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/AH-mahmoodnia/greenlight/internal/data"
 	_ "github.com/lib/pq"
 )
 
@@ -34,6 +35,7 @@ type config struct {
 type application struct {
 	config config
 	logger *log.Logger
+	models data.Models
 }
 
 func main() {
@@ -61,6 +63,7 @@ func main() {
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	srv := &http.Server{
---
+++ internal/data/models.go
@@ -0,0 +1,32 @@
+package data
+
+import (
+	"database/sql"
+	"errors"
+)
+
+var ErrRecordNotFound = errors.New("record not found")
+
+type Models struct {
+	Movies interface {
+		Insert(movie *Movie) error
+		Get(id int64) (*Movie, error)
+		Update(movie *Movie) error
+		Delete(id int64) error
+	}
+}
+
+// Return a Models struct containing the initialized MovieModel.
+func NewModels(db *sql.DB) Models {
+	return Models{
+		Movies: MovieModel{DB: db},
+	}
+}
+
+// ----------------------------------------------------------------
+
+func NewMockModels() Models {
+	return Models{
+		Movies: MockMovieModel{},
+	}
+}
