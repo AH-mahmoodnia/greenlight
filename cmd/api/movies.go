64360fa494ddb91da5880ae2e678dfe3f11beb40
amir mahmoodnia <mahmoodnia.am@gmail.com>
2023-05-22 18:52:22 +0330

    add some validation step to our query string.

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -193,6 +193,9 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 	input.Page = app.readInt(qs, "page", 1, v)
 	input.PageSize = app.readInt(qs, "page_size", 20, v)
 	input.Sort = app.readString(qs, "sort", "id")
+	input.SortSafeList = []string{"id", "title", "year", "runtime", "-id", "-title", "-year", "-runtime"}
+
+	data.ValidateFilters(v, input.Filters)
 
 	if !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
